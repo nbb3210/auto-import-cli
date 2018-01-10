dfd0339f6efc3f416cd4d63975c81c632b795911
徐强 <xuqiang@xuqiangdeMacBook-Pro.local>
2018-01-10 22:38:07 +0800

    编写样例文件

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        About
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+</style>
