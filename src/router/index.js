a5875ac6c73652ce504a3b22e141c36e14fe5b02
徐强 <xuqiang@xuqiangdeMacBook-Pro.local>
2018-01-11 01:08:35 +0800

    原始版本

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/* autoImport */
+const About = () => import('../pages/About.vue')
+const Home = () => import('../pages/Home.vue')
+/* autoImport */
+
+Vue.use(Router)
